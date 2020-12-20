036852724cbf2ee0be54cf6cf71ee544fda75b57
yano1996 <marcosyano@live.com.pt>
2020-12-20 19:50:11 -0300

    Angular 1.3

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,9 +1,11 @@
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import {NgModule} from "@angular/core";
 import {PhotosComponent} from "./photo/photo.component";
 
 @NgModule({
   declarations: [PhotosComponent],
-  exports: [ PhotosComponent ]
+  exports: [ PhotosComponent ],
+  imports: [HttpClientModule]
 })
 
 export class PhotosModule {}
