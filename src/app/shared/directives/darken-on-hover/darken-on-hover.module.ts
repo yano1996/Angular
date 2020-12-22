d171ef3881a823c07f40731e4c48613f3f978aee
yano1996 <marcosyano@live.com.pt>
2020-12-22 00:45:30 -0300

    Angular 1.7 Final

---
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { DarkOnHoverDirective } from './darken-on-hover.directive';
+
+
+@NgModule({
+  declarations:[DarkOnHoverDirective],
+  exports:[DarkOnHoverDirective]
+})
+
+export class DarkenOnHoverModule{}
